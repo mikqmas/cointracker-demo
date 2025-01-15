458b285dd60138ada990125d0122b867bfd28d45
samiyokim <samiyokim@gmail.com>
2025-01-14 19:44:46 -0800

    api routes for signup and login

--- server/main.py
+++ server/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
@@ -29,6 +29,37 @@ with app.app_context():
 @app.route('/')
 def index():
     return 'Welcome to the CoinTracker-Demo!'
+
+@app.route('/api/users/signup', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    try:
+        password = data.get('password')
+        # contrived password validation (in conjunction with FE validation)
+        if not password or len(password) < 4:
+            raise ValueError("Password not valid")
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        new_user = User(username=data['username'], password_hash=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return jsonify({"message": "User added successfully"}), 201
+    except Exception as e:
+        # server log for debugging, TODO: add to logging and monitoring Datadog/AWS Cloudwatch 
+        print(f"Error: {e}")
+        db.session.rollback()
+        
+        return jsonify({"error": "Failed to create user", "details": str(e)}), 401
+
+@app.route('/api/users/login', methods=['POST'])
+def login_user():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and bcrypt.check_password_hash(user.password_hash, data['password']):
+        return jsonify({"message": "Login successful", "username": user.username}), 200
+    else:
+        return jsonify({"error": "Invalid credentials"}), 401
 
 # for testing only
 @app.route("/api/users", methods=['GET'])
