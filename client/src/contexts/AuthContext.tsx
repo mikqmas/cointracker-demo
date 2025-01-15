07f86404e115e9a507a6a83b7c52279ae4b996d1
samiyokim <samiyokim@gmail.com>
2025-01-14 21:22:28 -0800

    add client side auth context provider

---
+++ client/src/contexts/AuthContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useState, useContext, ReactNode } from 'react';
+import axios from 'axios';
+
+const defaultUser = {}
+const AuthContext = createContext(defaultUser);
+
+export const useAuth = () => useContext(AuthContext);
+
+interface AuthProviderProps {
+    children: ReactNode
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+    const [userID, setUserID] = useState(null);
+    const [token, setToken] = useState(localStorage.getItem('token') || '');
+
+    const login = async (username: string, password: string) => {
+        try {
+            const response = await axios.post('http://localhost:8080/api/users/login', { username, password });
+            const { access_token, user_id } = response.data;
+            if (!access_token || !user_id) {
+                throw Error(`access token: ${access_token}, userid: ${user_id}`)
+            }
+            setUserID(user_id);
+            setToken(access_token);
+            localStorage.setItem('token', access_token);
+            localStorage.setItem('user_id', user_id);
+            return true;
+        } catch (error) {
+            console.error("Login failed:", error);
+            return false;
+        }
+    };
+
+    const logout = () => {
+        setUser(null);
+        setToken('');
+        localStorage.removeItem('token');
+        localStorage.removeItem('user_id');
+    };
+
+    const value = {
+        userID,
+        token,
+        login,
+        logout,
+    };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
